31babaa5810e6e0c9abe5b276fbff2a3b5a7c8a6
Bruno Guillaume <Bruno.Guillaume@loria.fr>
2021-10-13 17:20:02 +0200

    remove the "_meta" sublevel in compile

--- src/grew_main.ml
+++ src/grew_main.ml
@@ -191,15 +191,12 @@ let grep () = handle
 let compile () =
   handle
     (fun () ->
-       let grew_match = match !Grew_args.grew_match_server with
-         | Some dir -> Some (Filename.concat dir "_meta")
-         | None -> None in
        List.iter
          (fun json_file ->
             let corpus_desc_list = Corpus_desc.load_json json_file in
             List.iter
               (fun corpus_desc ->
-                 Corpus_desc.compile ~force:!Grew_args.force ?grew_match corpus_desc
+                 Corpus_desc.compile ~force:!Grew_args.force ?grew_match:!Grew_args.grew_match_server corpus_desc
               ) corpus_desc_list
          ) !Grew_args.input_data
     ) ()
