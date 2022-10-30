922ab519e71092c0934e66483816141a56c6c77d
Bruno Guillaume <Bruno.Guillaume@loria.fr>
2022-10-30 23:23:39 +0100

    Fix count test (tsv and json)

--- test/count/test.py
+++ test/count/test.py
@@ -5,8 +5,14 @@ sys.path.append(os.getcwd() + '/..')
 from utils import *
 
 print ("--------------- count 01 ---------------")
-success("grew_dev compile -i en_fr_zh.json && grew_dev count -patterns \"ADJ_NOUN.pat NOUN_ADJ.pat\" -i en_fr_zh.json", "01_expected.tsv")
+success("grew_dev compile -i en_fr_zh.json && grew_dev count -tsv -patterns \"ADJ_NOUN.pat NOUN_ADJ.pat\" -i en_fr_zh.json", "01_expected.tsv")
 
 print ("--------------- count 02 ---------------")
-success("grew_dev compile -i en_fr_zh.json && grew_dev count -patterns ADJ_NOUN.pat -key N.Number -i en_fr_zh.json", "02_expected.tsv")
+success("grew_dev compile -i en_fr_zh.json && grew_dev count -patterns \"ADJ_NOUN.pat NOUN_ADJ.pat\" -i en_fr_zh.json", "02_expected.json")
+
+print ("--------------- count 03 ---------------")
+success("grew_dev compile -i en_fr_zh.json && grew_dev count -tsv -patterns ADJ_NOUN.pat -key N.Number -i en_fr_zh.json", "03_expected.tsv")
+
+print ("--------------- count 04 ---------------")
+success("grew_dev compile -i en_fr_zh.json && grew_dev count -patterns ADJ_NOUN.pat -key N.Number -i en_fr_zh.json", "04_expected.json")
 
